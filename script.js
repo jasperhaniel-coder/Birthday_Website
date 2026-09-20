bd984896fa320b14e8d9f19368438b1415e23b02
Ebuka Jasper Emmanuel <jasperhaniel@gmail.com>
2026-09-20 08:51:35 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const surpriseBtn = document.getElementById("surpriseBtn");
+const surpriseMessage = document.getElementById("surpriseMessage");
+
+surpriseBtn.addEventListener("click", function () {
+
+    surpriseMessage.textContent =
+        "Your biggest gift is coming... A better version of me. ❤️🎂";
+
+});
